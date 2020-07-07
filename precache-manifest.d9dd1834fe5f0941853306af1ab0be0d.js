cef570441d8b551e510d7f4b983ac30122a9816c
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-07 21:10:10 +0300

    Updates

---
+++ precache-manifest.d9dd1834fe5f0941853306af1ab0be0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8b5a19956ca2321513e4c98e8bd05d",
+    "url": "/grid-calculator/index.html"
+  },
+  {
+    "revision": "608370881129346790f8",
+    "url": "/grid-calculator/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3e603fd11d131baa4417",
+    "url": "/grid-calculator/static/css/main.9962955a.chunk.css"
+  },
+  {
+    "revision": "608370881129346790f8",
+    "url": "/grid-calculator/static/js/2.7014a5ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/grid-calculator/static/js/2.7014a5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e603fd11d131baa4417",
+    "url": "/grid-calculator/static/js/main.8bbea66b.chunk.js"
+  },
+  {
+    "revision": "d0430a180106e60541b8",
+    "url": "/grid-calculator/static/js/runtime-main.487edb65.js"
+  }
+]);
